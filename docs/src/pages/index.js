649f2fa62a6d289a7adf1cee657ee1f7687ca471
Saiem Gilani <saiem.gilani@gmail.com>
2021-07-06 09:40:17 -0400

    add nflfastR endpoints

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -43,7 +43,7 @@ const FeatureList = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--6', styles.feature)}>
+    <div className={clsx('col col--4', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
