8d982b5bf492dab514bae34a46aab407a61c1d7f
Saiem Gilani <saiem.gilani@gmail.com>
2021-09-28 15:20:03 -0400

    roster link update

--- cfbfastR/nfl/__init__.py
+++ cfbfastR/nfl/__init__.py
@@ -80,14 +80,11 @@ def load_nfl_player_stats() -> pd.DataFrame:
 
     return data
 
-def load_nfl_rosters(seasons: List[int]) -> pd.DataFrame:
-    """Load NFL roster data
+def load_nfl_rosters() -> pd.DataFrame:
+    """Load NFL roster data for all seasons
 
     Example:
         `nfl_df = cfbfastR.nfl.load_nfl_rosters(seasons=range(1999,2021))`
-
-    Args:
-        seasons (list): Used to define different seasons. 1999 is the earliest available season.
 
     Returns:
         pd.DataFrame: Pandas dataframe containing rosters available for the requested seasons.
@@ -96,13 +93,8 @@ def load_nfl_rosters(seasons: List[int]) -> pd.DataFrame:
         ValueError: If `season` is less than 1999.
     """
     data = pd.DataFrame()
-    if type(seasons) is int:
-        seasons = [seasons]
-    for i in seasons:
-        if int(i) < 1999:
-            raise SeasonNotFoundError("season cannot be less than 1999")
-        i_data = pd.read_parquet(NFL_ROSTER_URL.format(season = i), engine='auto', columns=None)
-        data = data.append(i_data)
+
+    data = pd.read_csv(NFL_ROSTER_URL, compression='gzip', error_bad_lines=False, low_memory=False)
     #Give each row a unique index
     data.reset_index(drop=True, inplace=True)
 
